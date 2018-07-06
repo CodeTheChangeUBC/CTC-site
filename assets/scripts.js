86e6aa937446f2bc77eeb81062066ba637925fa8
Viniel Kumar <vinielk@icloud.com>
2018-07-06 13:08:37 -0700

    added smooth scrolling to work with us page

--- assets/scripts.js
+++ assets/scripts.js
@@ -5,14 +5,20 @@
 let activeElement = null;
 
 $(".project-container").click(function () {
-    if (activeElement) {
-        $(activeElement).removeClass("activeProject");
-    }
-    if (this) {
-        activeElement=this;
-    }
-    $(this).addClass("activeProject");
-    console.log(this.classList);
-    console.log(activeElement);
-}
-)
+   if (activeElement) {
+      $(activeElement).removeClass("activeProject");
+   }
+   if (this) {
+      activeElement=this;
+   }
+   $(this).addClass("activeProject");
+   console.log(this.classList);
+   console.log(activeElement);
+});
+
+// smooth scroll
+$(document).on('click', 'a[href^="#"]', function (event) {
+  $('html, body').animate({
+    scrollTop: $($.attr(this, 'href')).offset().top
+  }, 500);
+});
