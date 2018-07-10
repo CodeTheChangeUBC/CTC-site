a7d6be872892aee4dff6c018e2737264d0ce4cb2
sumeetgrewal <sumeetsinghgrewal@gmail.com>
2018-07-10 15:05:15 -0700

    Finished projects page

--- assets/scripts.js
+++ assets/scripts.js
@@ -2,14 +2,44 @@
 // Scripts
 //
 
-// smooth scroll
-$(document).on('click', '.workBanner a[href^="#"]', function (event) {
-  $('html, body').animate({
-    scrollTop: $($.attr(this, 'href')).offset().top
-  }, 500);
-});
 
 // projects page
+
+let act = 0;
+
+
+$(document).on('click', 'a[href^="#"]', function (event) {
+   if (act == 0) {
+      act = $($.attr(this, 'href'));
+      console.log(act);
+      $('html, body').animate({
+         scrollTop: $($.attr(this, 'href')).offset().top -= 100
+      }, 500);
+   } 
+
+});
+
+
 $('.show-more-button').on('click', function (event) {
+   
+   if (act != 0) {
+      $('.collapse.show').collapse('toggle');
+   }
    $('.collapse.show').collapse('toggle');
+});
+
+
+
+$('.closeIcon').on('click', function () {
+   $('.collapse.show').collapse('toggle');
+   act = 0;
+});
+
+
+// smooth scroll
+// WORK Banner
+$(document).on('click', '.workBanner a[href^="#"]', function (event) {
+   $('html, body').animate({
+      scrollTop: $($.attr(this, 'href')).offset().top
+   }, 500);
 });
