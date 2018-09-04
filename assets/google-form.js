83a3b987c93b6e1ca064b1855171cef762d29f49
Pritpal Chauhan <pchauhan@Pritpals-MacBook-Pro.local>
2018-09-04 00:54:11 -0700

    Volunteer link and general inquiries form

---
+++ assets/google-form.js
@@ -0,0 +1,27 @@
+!function(exports) {
+  exports.submitGoogleForm = submitGoogleForm;
+
+  function submitGoogleForm(form, cb) {
+    try {
+      var data = [].slice.call(form).map(function(control) {
+        return 'value' in control && control.name ?
+          control.name + '=' + (control.value === undefined ? '' : control.value) :
+          '';
+      }).join('&');
+      var xhr = new XMLHttpRequest();
+
+      xhr.open('POST', form.action + '/formResponse', true);
+      xhr.setRequestHeader('Accept',
+          'application/xml, text/xml, */*; q=0.01');
+      xhr.setRequestHeader('Content-type',
+          'application/x-www-form-urlencoded; charset=UTF-8');
+      xhr.send(data);
+
+      cb();
+    } catch(e) {}
+
+    form.parentNode.className += ' submitted';
+
+    return false;
+  }
+}(typeof module === 'undefined' ? window : module.exports);
--- assets/scripts.js
+++ assets/scripts.js
@@ -15,13 +15,13 @@ $(document).on('click', 'a[href^="#"]', function (event) {
       $('html, body').animate({
          scrollTop: $($.attr(this, 'href')).offset().top -= 100
       }, 500);
-   } 
+   }
 
 });
 
 
 $('.show-more-button').on('click', function (event) {
-   
+
    if (act != 0) {
       $('.collapse.show').collapse('toggle');
    }
@@ -35,6 +35,35 @@ $('.closeIcon').on('click', function () {
    act = 0;
 });
 
+// Contact Us Form
+$(document).on('submit', '#contactForm', function (event) {
+  let thankYou = $(".thank-you");
+  thankYou.show();
+  thankYou.fadeOut(15000);
+  return window.submitGoogleForm(this, function() {
+    $("#contactForm")[0].reset();
+  });
+});
+
+// Partner With Us Form
+$(document).on('submit', '#partner-form', function (event) {
+  let thankYou = $(".thank-you");
+  thankYou.show();
+  thankYou.fadeOut(15000);
+  return window.submitGoogleForm(this, function() {
+    $("#partner-form")[0].reset();
+  });
+});
+
+// Volunteer With Us Form
+$(document).on('submit', '#volunteer-form', function (event) {
+  let thankYou = $(".thank-you");
+  thankYou.show();
+  thankYou.fadeOut(15000);
+  return window.submitGoogleForm(this, function() {
+    $("#volunteer-form")[0].reset();
+  });
+});
 
 // smooth scroll
 // WORK Banner
